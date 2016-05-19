f425b9956b134ecf9cd8be34f72474247e95bd76
pejo1 <per.jonsson@betssongroup.com>
2016-05-19 16:05:41 +0200

    feat(init): getting started
    example updated

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # cycle-preybird
 Experimental cycle.js project
+
+## Getting started
+
+### Installation
+> npm install
+
+### Run
+> webpack
+
+### Develop
+> webpack --watch
+
+## About
+Author: [ProgrammerPer](https://www.programmerper.com)
--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 import Cycle from '@cycle/core';
-import {div, label, input, hr, h1, makeDOMDriver} from '@cycle/dom';
+// import {div, label, input, hr, h1, makeDOMDriver} from '@cycle/dom';
+import CycleDOM from '@cycle/dom';
 
-function main(sources) {
-    const sinks = {
-        DOM: sources.DOM.select('.field').events('input')
-            .map(ev => ev.target.value)
-            .startWith('')
-            .map(name =>
-                div([
-                    label('Name:'),
-                    input('.field', {attributes: {type: 'text'}}),
-                    hr(),
-                    h1('Hello ' + name)
-                ])
-            )
+//
+// function main(sources) {
+//     const sinks = {
+//         DOM: sources.DOM.select('.field').events('input')
+//             .map(ev => ev.target.value)
+//             .startWith('')
+//             .map(name =>
+//                 div([
+//                     label('Name:'),
+//                     input('.field', {attributes: {type: 'text'}}),
+//                     hr(),
+//                     h1('Hello ' + name)
+//                 ])
+//             )
+//     };
+//     return sinks;
+// }
+//
+// Cycle.run(main, { DOM: makeDOMDriver('#app-container') });
+
+function main() {
+    return {
+        DOM: Rx.Observable.interval(1000)
+            .map(i => CycleDOM.h1('' + i + ' seconds elapsed'))
     };
-    return sinks;
 }
 
-Cycle.run(main, { DOM: makeDOMDriver('#app-container') });
+const drivers = {
+    DOM: CycleDOM.makeDOMDriver('#app')
+};
+
+Cycle.run(main, drivers);
